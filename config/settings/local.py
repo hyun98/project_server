bc8abbbca5e80891ca25f579a268840bf85997a2
Henu <hyun0404woo@gmail.com>
2021-09-12 02:27:00 +0900

    secret key bugfix

--- config/settings/local.py
+++ config/settings/local.py
@@ -3,8 +3,6 @@ from .base import *
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- config/settings/prod.py
+++ config/settings/prod.py
@@ -14,8 +14,6 @@ def read_secret(secret_name):
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
