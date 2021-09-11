069baad15ce33aa77274a088f82e8fbbb2900d11
Henu <hyun0404woo@gmail.com>
2021-09-12 02:43:58 +0900

    env path

--- config/settings/base.py
+++ config/settings/base.py
@@ -16,7 +16,7 @@ from pathlib import Path
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 env = environ.Env(
     DEBUG=(bool, False)
