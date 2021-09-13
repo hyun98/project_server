f09f8926f773d0da0119add9ac29b624a4b604a8
Henu <hyun0404woo@gmail.com>
2021-09-13 16:41:01 +0900

    staticroot path

--- config/settings/base.py
+++ config/settings/base.py
@@ -164,7 +164,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, '/static/staticroot')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticroot')
 
 
 MEDIA_URL = '/media/'
