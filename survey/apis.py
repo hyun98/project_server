1a076363652e2eccc34aa589f4060f3c052bd4ae
Henu <hyun0404woo@gmail.com>
2021-12-20 14:33:27 +0900

    due date

--- survey/apis.py
+++ survey/apis.py
@@ -2,6 +2,7 @@ import urllib
 import os
 import json
 import mimetypes
+from datetime import datetime
 
 from rest_framework import status
 from rest_framework.views import APIView
@@ -387,7 +388,7 @@ class ApplierSelfCheckApi(PublicApiMixin, APIView):
         applier = applier.first()
         
         today = datetime.datetime.today()
-        due_date = str(survey.due_date) + " 00:00:00"
+        due_date = str(survey.due_date) + " 23:59:59"
         due_date = datetime.datetime.strptime(due_date, "%Y-%m-%d %H:%M:%S")
         applier_data = {}
         applier_data["due_date"] = survey.due_date
